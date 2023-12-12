47ac944258dfa8670fcaa722474a0242af1c213f
Marcos Vinícius <marcosbailoni@hotmail.com>
2023-12-12 15:30:02 -0300

    Classe: Construtores / Métodos
    Adicionado conteúdo no pacote Classe.
    Conteúdos sobre Construtores e métodos e Objetos.

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/8e/704c68fffc98001e1eeea5866719441a
@@ -0,0 +1,9 @@
+package Classe;
+
+public class Data {
+
+	int dia;
+	int mes;
+	int ano;
+	
+}
--- udemyAulas/src/Classe/Data.java
+++ udemyAulas/src/Classe/Data.java
@@ -6,4 +6,24 @@ public class Data {
 	int mes;
 	int ano;
 	
+//	Construtor padrão setando valores padrões para quando não passar parâmetros
+	Data() {
+		dia = 1;
+		mes = 1;
+		ano = 1970;
+	}
+	
+//	Construtor recebendo os 3 parâmetros
+	Data(int dia, int mes, int ano) {
+		this.dia = dia;
+		this.mes = mes;
+		this.ano = ano;	
+	}
+	
+	
+	String dataFormatada () {
+//		os dois returns funcionam: 
+//		return dia + "/" + mes +  "/" + ano;
+		return String.format("%d/%d/%d", dia, mes, ano);
+	}
 }
