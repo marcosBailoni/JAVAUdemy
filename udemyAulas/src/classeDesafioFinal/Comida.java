fba2c568cab7843d4585df04ca56739c6c1eec6b
Marcos Vinícius <marcosbailoni@hotmail.com>
2023-12-14 16:21:16 -0300

    Conclusão Classes
    Conclusão do módulo de Classes e Métodos;

---
+++ udemyAulas/src/classeDesafioFinal/Comida.java
@@ -0,0 +1,19 @@
+package classeDesafioFinal;
+
+public class Comida {
+
+	String nome;
+	double peso;
+	
+//	Construtores
+	
+	Comida () {
+		
+	}
+	
+	Comida (String nome, double peso){
+		this.nome = nome;
+		this.peso = peso;
+	}
+	
+}
