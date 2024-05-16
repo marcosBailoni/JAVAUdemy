1184fd8899e1490f2b09794656e2fd5452b6d976
Marcos Vinícius <marcosbailoni@hotmail.com>
2024-05-16 16:42:41 -0300

    More generics uses

---
+++ udemyAulas/src/generics/CaixaNumero.java
@@ -0,0 +1,6 @@
+package generics;
+
+// Definindo o tipo genérico para que seja limitado para Objetos herdados de Number (Double, Integer, etc..) 
+public class CaixaNumero<N extends Number> extends CaixaGenerics<N>{
+
+}
---
+++ udemyAulas/src/generics/Explicacao.java
@@ -0,0 +1,12 @@
+package generics;
+
+public class Explicacao {
+
+	
+//	Por padrão, ao criar uma classe ela herda de objeto, mas é possível criar uma classe sem essa herança.
+//	Dessa forma, você pode fazer com que uma variável ou atributo de uma classe, só seja definida no momento da criação da instãncia. 
+//	Com isso, não precisa fazer o CAST(conversão) dos atributos. 
+//	Por exemplo, uma lista de objetos, recebe um index 0 = "Ola Mundo", ao armazenar o indice 0 em outra váriavel, precisa fazer o cast.
+// ex - String a = (String) lista.get(0); no generics, a lista teria sido criado como <String>, para não ter que fazer o cast.
+	
+}
